a0099cd39f095c89fb9522c68a410481e25da1fd
YeisonSegura <yeisonrinconcr7@gmail.com>
2026-05-27 11:54:40 -0500

    Url

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'https://gimnasio-backend-18ar.onrender.com/api',
+  baseURL: import.meta.env.VITE_API_URL || 'https://gimnasio-version2.onrender.com',
 })
 
 export default api
